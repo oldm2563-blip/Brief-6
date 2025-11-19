2931df81caf393af56379518339bbac34185f71a
oldman <oldm2563@email.com>
2025-11-19 17:14:23 +0100

    adding person's info

--- script.js
+++ script.js
@@ -29,9 +29,47 @@ add.addEventListener("click", ()=>{
     const newempl = document.createElement("div")
     newempl.classList.add("employee");
     newempl.innerHTML = `<h3>${thename} => ${role}</h3> <button class="edit">Edit</button></div>`
-
     employees.appendChild(newempl);
-
     form.style.display = "none";
     background.style.display = "none";
+
+
+    name.value = "";
+    email.value = "";
+    number.value = "";
+    pic.value = "";
+    select.value = "Manager"
+
+
+
+    newempl.addEventListener("click" , () =>{
+        const info = document.getElementById("info");
+        const showinfo = document.createElement("div");
+        background.style.display = "block";
+        info.style.display = "block"
+        showinfo.innerHTML = `
+        <div class = "theinfo">
+        <h1>Name :</h1>
+        <h3>${thename}</h2> 
+        <h1>Email :</h1>
+        <h2>${themail}</h2> 
+        <h1>Number :</h1>
+        <h2>${num}</h2>
+        <h1>Role :</h1>
+        <h2>${role}</h2> 
+        </div>
+        `
+        const quit = document.createElement("button")
+        quit.classList.add("quit");
+        quit.textContent = "Hide";
+        quit.addEventListener("click", ()=>{
+            background.style.display = "none";
+            info.style.display = "none";
+            showinfo.remove()
+            quit.remove()
+        })
+        info.appendChild(showinfo);
+        info.appendChild(quit);
+
+    })
 })
